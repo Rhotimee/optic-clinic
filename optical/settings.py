7783aedb914f9a1bbf5c2d22d66a2c50e95638a6
Yemitan Isaiah Rotimi <isaroy007@gmail.com>
2018-01-22 00:14:54 +0100

    start app prescription

--- optical/settings.py
+++ optical/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Lagos'
 
 USE_I18N = True
 
