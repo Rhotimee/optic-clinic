ba43994c7004811ea3fd96a71f4d946bd936efdf
Yemitan Isaiah Rotimi <isaroy007@gmail.com>
2018-01-22 09:49:30 +0100

    models for Prescription, Patients records and Doctors

--- clinic/admin.py
+++ clinic/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Patient, Doctor, Prescription
 
-# Register your models here.
+
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'phone_number')
+
+
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'phone_number')
+
+
+class PrescriptionAdmin(admin.ModelAdmin):
+    list_display = ['patient', 'left_sph', 'left_cyl', 'left_axis', 'left_add', 'right_sph', 'right_cyl', 'right_axis', 'right_add']
+    list_filter = ('doctor', 'patient', 'date')
+    fields = [('patient', 'doctor'), ('left_sph', 'left_cyl', 'left_axis', 'left_add'),
+              ('right_sph', 'right_cyl', 'right_axis', 'right_add'),
+              ('photochromic', 'no_prescription'),
+              'date'
+              ]
+
+admin.site.register(Patient, PatientAdmin)
+admin.site.register(Doctor, DoctorAdmin)
+admin.site.register(Prescription, PrescriptionAdmin)
--- clinic/models.py
+++ clinic/models.py
@@ -1,3 +1,77 @@
+from django.core.validators import RegexValidator
 from django.db import models
 
-# Create your models here.
+
+class Patient(models.Model):
+    gender_choices = (('male', 'male'),
+                      ('female', 'female'))
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    other_name = models.CharField(max_length=20, blank=True, null=True)
+    gender = models.CharField(choices=gender_choices, max_length=7)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    dob = models.DateField(null=True, blank=True)
+    age = models.IntegerField(null=True, blank=True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message=
+                                 "Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)  # validators should be a list
+    occupation = models.CharField(max_length=30, blank=True, null=True)
+    address = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField()
+
+    class Meta:
+        ordering = ["-timestamp"]
+
+    def __str__(self):
+        return '{0}, {1}'.format(self.last_name, self.first_name)
+
+
+class Doctor(models.Model):
+    gender_choices = (('male', 'male'),
+                      ('female', 'female'))
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    other_name = models.CharField(max_length=20, blank=True, null=True)
+    gender = models.CharField(choices=gender_choices, max_length=7)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    dob = models.DateField(null=True, blank=True)
+    age = models.IntegerField(null=True, blank=True)
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
+                                 message=
+                                 "Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)  # validators should be a list
+    address = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField()
+
+    class Meta:
+        ordering = ["-timestamp"]
+
+    def __str__(self):
+        return '{0}, {1}'.format(self.last_name, self.first_name)
+
+
+class Prescription(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    left_sph = models.CharField(max_length=6, null=True, blank=True)
+    left_cyl = models.CharField(max_length=6, null=True, blank=True)
+    left_axis = models.CharField(max_length=6, null=True, blank=True)
+    left_add = models.CharField(max_length=6, null=True, blank=True)
+    right_sph = models.CharField(max_length=6, null=True, blank=True)
+    right_cyl = models.CharField(max_length=6, null=True, blank=True)
+    right_axis = models.CharField(max_length=6, null=True, blank=True)
+    right_add = models.CharField(max_length=6, null=True, blank=True)
+    photochromic = models.BooleanField(default=False)
+    comment = models.TextField(max_length=100, null=True, blank=True)
+    no_prescription = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    date = models.DateField()
+    doctor = models.ForeignKey(Doctor, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return self.patient.last_name
+
+
